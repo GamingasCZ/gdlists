8b70cc5d490a90b76082370cb9f9fcb029065425
GamingasCZ <gamingforyou875@gmail.com>
2023-07-28 11:52:17 +0200

    color picker slider overhaul

--- src/components/global/ColorPicker.vue
+++ src/components/global/ColorPicker.vue
@@ -41,3 +41,56 @@ watch(props, () => colors.value = [props.hue, props.saturation, props.lightness]
     </div>
   </section>
 </template>
+
+<style scoped>
+.colorPickerSlider {
+  appearance: none;
+  background: none;
+  transform: translateY(-0.2rem);
+  height: 2rem;
+}
+.colorPickerSlider::-moz-range-track {
+  appearance: none;
+  background: rgba(0, 0, 0, 0.43);
+  border: none;
+  outline: none;
+  height: 0.3rem;
+  border-radius: 0.25rem;
+}
+.colorPickerSlider::-moz-range-thumb {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  background: url("@/images/picker.svg");
+  width: 1.1rem;
+  height: 1.5rem;
+  background-size: cover;
+  transform: none;
+  transition: transform 0.05s ease-in-out;
+}
+
+.colorPickerSlider::-moz-range-thumb:active { transform: scale(1.2) translateY(-0.2rem); }
+
+.colorPickerSlider::-webkit-slider-runnable-track {
+  appearance: none;
+  background: rgba(0, 0, 0, 0.43);
+  border: none;
+  outline: none;
+  height: 0.3rem;
+  border-radius: 0.25rem;
+}
+.colorPickerSlider::-webkit-slider-thumb {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  background: url("@/images/picker.svg");
+  width: 1.25rem;
+  height: 1.8rem;
+  background-size: cover;
+  transform: translateY(-0.8rem);
+  transition: transform 0.05s ease-in-out;
+}
+
+.colorPickerSlider::-webkit-slider-thumb:active { transform: scale(1.1) translateY(-1rem); }
+
+</style>
