08149d93ad057c5bdfd97d66edfff72b25f6f4f5
GamingasCZ <gamingforyou875@gmail.com>
2024-08-13 22:25:00 +0200

    resizer gizmo works on mobile now

--- src/components/global/Resizer.vue
+++ src/components/global/Resizer.vue
@@ -20,12 +20,19 @@ const imageScale = ref(0)
 const gizmo = ref<HTMLButtonElement>()
 
 const mousePos = ref([0, 0])
-const trackPos = (e: MouseEvent) => mousePos.value = [e.clientX, e.clientY]
+const trackPos = (e: MouseEvent | TouchEvent) => {
+    if (e?.clientX) mousePos.value = [e.clientX, e.clientY]
+    else mousePos.value = [e.touches.item(0).screenX, e.touches.item(0).screenY]
+}
 var scaleID
 const startScale = () => {
     document.body.addEventListener("mouseup", endScale, {once: true})
     document.body.addEventListener("mousedown", trackPos)
     document.body.addEventListener("mousemove", trackPos)
+
+    document.body.addEventListener("touchmove", trackPos)
+    document.body.addEventListener("touchend", endScale, {once: true})
+    document.body.style.overflow = "clip"
     
     scaleID = setInterval(() => {
         if (!mousePos.value[0] || !mousePos.value[1]) return
@@ -41,14 +48,18 @@ const startScale = () => {
             imageScale.value = Math.min(Math.max(props.minSize ?? 96, self.value?.clientHeight + drag), props.maxSize ?? 1024)
         }
 
-        imageScale.value = Math.trunc(imageScale.value/10)*10
+        imageScale.value = Math.floor(imageScale.value/10)*10
 
         emit("resize", imageScale.value)
     }, 20)
 }
 
 const endScale = () => {
+    document.body.style.overflow = ""
+
     document.body.removeEventListener("mousemove", trackPos)
+    document.body.removeEventListener("mousedown", trackPos)
+    document.body.removeEventListener("mouseup", endScale, {once: true})
     clearInterval(scaleID)
 }
 
@@ -59,7 +70,7 @@ const endScale = () => {
         <div :class="{'p-1 border-2 border-transparent group-hover:border-blue-400': editable}">
             <slot :width="width" />
         </div>
-        <button v-if="editable" ref="gizmo" @mousedown="startScale" class="isolate absolute -bottom-1.5 w-4 h-4 bg-white rounded-full scale-0 group-hover:scale-100"
+        <button v-if="editable" ref="gizmo" @mousedown="startScale" @touchstart="startScale" class="isolate absolute -bottom-1.5 w-4 h-4 bg-white rounded-full scale-0 group-hover:scale-100"
         :class="{'-right-1': gizmoPos == 'corner', 'left-1/2': gizmoPos == 'vertical'}"
         ></button>
     </div>
