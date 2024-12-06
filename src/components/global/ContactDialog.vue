e1e4aff4e0d59b8375725ba248b6e8e1f1032973
GamingasCZ <gamingforyou875@gmail.com>
2024-12-06 10:53:19 +0100

    replaced twitter link with bsky in contact dialog

--- src/components/global/ContactDialog.vue
+++ src/components/global/ContactDialog.vue
@@ -14,8 +14,8 @@ const emit = defineEmits(['closePopup'])
             <span>@g4mingaz</span>
         </div>
         <div class="flex gap-6 p-3 bg-black bg-opacity-40 rounded-md">
-            <img src="@/images/socials/twitter.svg" class="mr-1 w-7" alt="">
-            <span>@geodlists</span>
+            <img src="@/images/socials/bluesky.svg" class="mr-1 w-7" alt="">
+            <span>@gamingas.cz</span>
         </div>
         <div class="flex gap-6 p-3 underline bg-black bg-opacity-40 rounded-md">
             <img src="@/images/mail.svg" class="w-8" alt="">
