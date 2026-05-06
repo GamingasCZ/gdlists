c2c0d505e9978d4e51574ef9278a840bcc3c6e30
GamingasCZ <gamingforyou875@gmail.com>
2026-05-06 19:55:41 +0200

    hide draft card empty message, when draft has only title

--- src/components/writer/DraftCardSmall.vue
+++ src/components/writer/DraftCardSmall.vue
@@ -25,7 +25,7 @@ const emit = defineEmits<{
         <hr class="opacity-20">
         <p :style="{mask: 'linear-gradient(white, #FFFFFF20)'}">
             <p class="text-sm font-bold">{{ draft.previewTitle }}</p>
-            <p v-if="draft.previewParagraph.length" class="text-xs" v-html="parseText(draft.previewParagraph, true)"></p>
+            <p v-if="draft.previewParagraph.length || draft.previewTitle.length" class="text-xs" v-html="parseText(draft.previewParagraph, true)"></p>
             <p v-else class="text-xs text-white text-opacity-40">{{ $t('reviews.draftNoPrev') }}</p>
         </p>
         <button class="absolute bottom-2 left-1/2 p-2 w-max bg-black bg-opacity-60 rounded-sm opacity-0 transition-opacity duration-75 -translate-x-1/2 button group-hover:opacity-100" @click.stop="emit('previewDraft')">
